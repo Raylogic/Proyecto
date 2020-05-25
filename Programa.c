94b099a3cceb18266c44de3c39c61c1140e35b34
Raymundo <A00570654@itesm.mx>
2020-05-24 20:59:19 -0500

    Versión inicial

--- Programa.c
+++ Programa.c
@@ -6,12 +6,12 @@ int main(){
    int a = 12;
    int b = 3;
 
-   int op1 = a + b;
+   int op1 = suma(a,b);
    printf("%i",op1);
-   int op2 = a - b; 
+   int op2 = resta(a,b);
    printf("%i",op2);
-   int op3 = a * b;
+   int op3 = multi(a,b);
    printf("%i",op3);
-   
-   return 0;
+
+return 0;
 }
