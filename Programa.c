dca65ad5024f4b929e809b903b12059ac796e4d5
Raymundo <A00570654@itesm.mx>
2020-05-24 20:20:38 -0500

    Versión inicial

---
+++ Programa.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+   int a = 12;
+   int b = 3;
+
+   int op1 = a + b;
+   printf("%i",op1);
+   int op2 = a - b; 
+   printf("%i",op2);
+   int op3 = a * b;
+   printf("%i",op3);
+   
+   return 0;
+}
